6ba92ef5ed434c810c4d431d07c64ba4fb33391d
Garrett Johnson <mavenvisual@gmail.com>
2021-01-25 18:46:04 -0500

    error fix 3

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function complete() {
 
  // Get Quote From API
    async function getQuote () {
-    loading();
+       loading();
        const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
        const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
        try {
